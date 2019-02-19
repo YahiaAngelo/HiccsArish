13bf1511a8b06ac5979eac1151cd7029d5b9b080
fatma <fatma_s3@yahoo.com>
2019-02-19 15:43:51 -0800

    home activities and intents

---
+++ app/src/main/java/com/hiccs/arish/activities/ExtraCoursesActivity.java
@@ -0,0 +1,15 @@
+package com.hiccs.arish.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hiccs.arish.R;
+
+public class ExtraCoursesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extra_courses);
+    }
+}
--- app/src/main/java/com/hiccs/arish/activities/HomeActivity.java
+++ app/src/main/java/com/hiccs/arish/activities/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.hiccs.arish.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.hiccs.arish.R;
 
@@ -11,5 +13,35 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    public void student(View view) {
+        Intent i = new Intent(HomeActivity.this, StudentActivity.class);
+        startActivity(i);
+    }
+
+    public void Gallery(View view) {
+        Intent i = new Intent(HomeActivity.this, GalleryActivity.class);
+        startActivity(i);
+    }
+
+    public void staff(View view) {
+        Intent i = new Intent(HomeActivity.this, StaffActivity.class);
+        startActivity(i);
+    }
+
+    public void news(View view) {
+        Intent i = new Intent(HomeActivity.this, NewsActivity.class);
+        startActivity(i);
+    }
+
+
+    public void extra_courses(View view) {
+        Intent i = new Intent(HomeActivity.this, ExtraCoursesActivity.class);
+        startActivity(i);
+    }
+
+    public void about(View view) {
+        Intent i = new Intent(HomeActivity.this, AboutActivity.class);
+        startActivity(i);
     }
 }
